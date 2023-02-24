e66c4dc2dcea2a6c628a48951ee35acda282c4dc
edbonamigo <edbmuller@gmail.com>
2023-02-24 09:45:25 -0300

    :wrench: Refactor __dirname for ES6 Modules

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import path from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 const port = 3000
