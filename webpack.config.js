958a89d838ef3a7ebec7e73509a55efd7422876d
edbonamigo <edbmuller@gmail.com>
2023-03-09 13:18:56 -0300

    :recycle: Refactor Scripts and Webpack dev/prod config

--- webpack.config.js
+++ webpack.config.js
@@ -10,123 +10,115 @@ import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin'
 import TerserPlugin from 'terser-webpack-plugin'
 import sass from 'sass'
 
-const IS_DEVELOPMENT = process.env.NOVE_ENV === 'dev'
+export default (env, argv) => {
+	const IS_DEVELOPMENT = argv.mode === 'development'
 
-const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
-const dirStyles = path.join(__dirname, 'styles')
-const disNode = 'node_modules'
+	return {
+		entry: {
+			bundle: path.resolve(__dirname, './app/index.js'),
+			main: path.resolve(__dirname, './styles/index.scss'),
+		},
 
-const config = {
-	entry: [
-		path.join(dirApp, 'index.js'), //
-		path.join(dirStyles, 'index.scss'),
-	],
-	resolve: {
-		modules: [
-			dirApp, //
-			dirAssets,
-			dirStyles,
-			disNode,
-		],
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			IS_DEVELOPMENT,
-		}),
-		new CopyWebpackPlugin({
-			patterns: [
-				{
-					from: './assets',
-					to: '',
-					noErrorOnMissing: true,
-				},
-			],
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css',
-		}),
-		new CleanWebpackPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-				},
+		output: {
+			path: path.resolve(__dirname, 'public'),
+			filename: '[name].js',
+		},
+
+		devtool: 'source-map',
+
+		devServer: {
+			devMiddleware: {
+				publicPath: '/public',
+				writeToDisk: true,
 			},
-			{
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: [
+		},
+
+		plugins: [
+			new webpack.DefinePlugin({
+				IS_DEVELOPMENT,
+			}),
+			new CopyWebpackPlugin({
+				patterns: [
 					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							url: false,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							implementation: sass,
-						},
+						from: './assets',
+						to: '',
+						noErrorOnMissing: true,
 					},
 				],
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
-				loader: 'file-loader',
-				options: {
-					name(file) {
-						return '[name].[ext]'
-					},
-				},
-			},
-			// You need this, if you are using `import file from "file.ext"`, for `new URL(...)` syntax you don't need it
-			{
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				type: 'asset',
-			},
-			{
-				test: /\.(glsl|frag|vert)$/,
-				loader: 'raw-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.(glsl|frag|vert)$/,
-				loader: 'glslify-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin(),
-			// We recommend using only for the "production" mode
-			new ImageMinimizerPlugin({
-				minimizer: {
-					implementation: ImageMinimizerPlugin.imageminMinify,
-					options: {
-						plugins: [
-							'imagemin-gifsicle',
-							'imagemin-mozjpeg',
-							'imagemin-pngquant',
-							'imagemin-svgo',
-						],
-					},
-				},
 			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].css',
+				chunkFilename: '[id].css',
+			}),
+			new CleanWebpackPlugin(),
 		],
-	},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					use: {
+						loader: 'babel-loader',
+					},
+				},
+				{
+					test: /\.scss$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+						},
+						{
+							loader: 'css-loader',
+							options: {
+								url: false,
+							},
+						},
+						{
+							loader: 'postcss-loader',
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								implementation: sass,
+							},
+						},
+					],
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+					loader: 'file-loader',
+					options: {
+						name(file) {
+							return '[name].[ext]'
+						},
+					},
+				},
+				// You need this, if you are using `import file from "file.ext"`, for `new URL(...)` syntax you don't need it
+				{
+					test: /\.(jpe?g|png|gif|svg)$/i,
+					type: 'asset',
+				},
+			],
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [
+				new TerserPlugin(),
+				// We recommend using only for the "production" mode
+				new ImageMinimizerPlugin({
+					minimizer: {
+						implementation: ImageMinimizerPlugin.imageminMinify,
+						options: {
+							plugins: [
+								'imagemin-gifsicle',
+								'imagemin-mozjpeg',
+								'imagemin-pngquant',
+								'imagemin-svgo',
+							],
+						},
+					},
+				}),
+			],
+		},
+	}
 }
-
-export default config
