fff79af4dc0945dfd87772acb2e566178db15bdc
edbonamigo <edbmuller@gmail.com>
2023-04-05 10:07:34 -0300

    :zap: Polished Preloader Animation

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -10,14 +10,16 @@ export default class Preloader extends Component {
 		super({
 			element: '.preloader',
 			elements: {
-				title: '.preloader__text',
+				text: '.preloader__text',
 				number: '.preloader__number',
 				numberText: '.preloader__number__text',
 				images: document.querySelectorAll('.js-lazy-load'),
+				title: '.hero__title',
+				subtitle: '.hero__subtitle',
 			},
 		})
 
-		split({ element: this.elements.title })
+		split({ element: this.elements.text })
 		this.spans = GSAP.utils.toArray('.preloader span')
 
 		this.i = 0
@@ -40,11 +42,11 @@ export default class Preloader extends Component {
 	onLoaded() {
 		return new Promise((resolve) => {
 			this.hidePreloader = GSAP.timeline({
-				delay: 2,
+				delay: 1,
 			})
 
 			this.hidePreloader.to([this.spans, this.elements.numberText], {
-				duration: 2,
+				duration: 1.6,
 				ease: 'expo.out',
 				stagger: 0.02,
 				autoAlpha: 0,
@@ -54,14 +56,37 @@ export default class Preloader extends Component {
 			this.hidePreloader.to(
 				this.element,
 				{
-					duration: 2,
+					duration: 1.2,
 					ease: 'expo.out',
+					autoAlpha: 0,
+				},
+				'-=0.2'
+			)
+
+			this.hidePreloader.from(
+				this.elements.title,
+				{
+					duration: 0.8,
+					ease: 'expo.out',
+					y: '10%',
 					autoAlpha: 0,
 				},
 				'-=1'
 			)
 
+			this.hidePreloader.from(
+				this.elements.subtitle,
+				{
+					duration: 0.8,
+					ease: 'expo.out',
+					y: '30%',
+					autoAlpha: 0,
+				},
+				'-=0.9'
+			)
+
 			this.hidePreloader.call((_) => {
+				document.documentElement.classList.remove('--preloader')
 				this.emit('completed')
 			})
 		})
